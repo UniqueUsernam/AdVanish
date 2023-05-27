68077006ea9d23f85f5c4cc69863b96ca4ddab09
O5Command <73913201+UniqueUsernam@users.noreply.github.com>
2023-05-27 18:02:42 -0400

    Delete AdVanish directory

--- AdVanish/mainInterface.js
+++
@@ -1,7 +0,0 @@
-// Support script for main.html, the main AdVanish interface. This script provides the Open Disable Page functionality.
-document.addEventListener("DOMContentLoaded", function () {
-	document.getElementById("disable").addEventListener("click", function () {
-		extensionID = chrome.runtime.id;
-		chrome.tabs.create({ url: "chrome://extensions/?id=" + extensionID });
-	});
-})
